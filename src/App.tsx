eaa12ad39c73f7ecaaec8a5c9675e8eca4dd2f02
aadil42 <adilkelawala@gmail.com>
2023-07-20 15:51:55 +0530

    adding Todos and newTodos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-
 import Todos from './components/Todos';
+import Todo from './models/Todo';
+import NewTodo from './components/NewTodo';
 
 function App() {
+
+  const myTodos = [
+    new Todo('item1'),
+    new Todo('item2')
+  ];
+
   return (
     <div className="App">
-      <Todos />
+      <Todos items={myTodos}/>
+      <NewTodo />
     </div>
   );
 }
